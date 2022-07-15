c455344563941aecd822ef31fbdf2f2fe934d3e2
pratikbaid3 <pratikbaid3@gmail.com>
2022-07-15 16:10:27 +0530

    feat: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles.css';
+import * as waxjs from '@waxio/waxjs/dist'; // development only
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const wax = new waxjs.WaxJS({
+        rpcEndpoint: 'https://wax.greymass.com',
+        userAccount: 'pptkq.c.wam',
+        tryAutoLogin: false,
+        pubKeys: [
+            'EOS6hMXgvPoyJyFLeji6TaMRg4dUSSsZCmdcKjnTzfHh65iM5zB3c',
+            'EOS8UhZSLGoiUSifugc4x2LrLbKW6GwKKNzJbxtZBBChqcKbfV18G',
+        ],
+    });
+
+    async function login() {
+        try {
+            const userAccount = await wax.login();
+            document.getElementById('response').append(userAccount);
+        } catch (e) {
+            document.getElementById('response').append(e.message);
+        }
+        alert('You have Succesfully loged in');
+    }
+
+    async function sign() {
+        if (!wax.api) {
+            return document
+                .getElementById('response')
+                .append('* Login first *');
+        }
+
+        try {
+            console.log(wax.userAccount);
+            console.log(wax.pubKeys);
+            const result = await wax.api.transact(
+                {
+                    actions: [
+                        {
+                            account: 'eosio.token',
+                            name: 'transfer',
+                            authorization: [
+                                {
+                                    actor: wax.userAccount,
+                                    permission: 'active',
+                                },
+                            ],
+                            data: {
+                                from: wax.userAccount,
+                                to: 'eosio',
+                                quantity: '0.00000001 WAX',
+                                memo: '',
+                            },
+                        },
+                    ],
+                },
+                {
+                    blocksBehind: 3,
+                    expireSeconds: 1200,
+                }
+            );
+            console.log(result);
+            document.getElementById('response').append(JSON.stringify(result));
+        } catch (e) {
+            document.getElementById('response').append(e.message);
+        }
+    }
+    return (
+        <div className='App'>
+            <button id='login' onClick={login}>
+                Wax login
+            </button>
+            <br />
+            <hr />
+            <button id='sign' className='btn btn-success' onClick={sign}>
+                Sign transaction
+            </button>
+            <h2 id='response'> </h2>
+
+            <p id='demo'></p>
+        </div>
+    );
 }
-
-export default App;
